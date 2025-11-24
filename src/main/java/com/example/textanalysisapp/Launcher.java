1d96a0d9864f18ed98d5768849115f034bac8dc2
DarinKhelifa <darinkhelifa@gmail.com>
2025-11-24 12:11:10 +0100

    team members

--- src/main/java/com/example/textanalysisapp/Launcher.java
+++ src/main/java/com/example/textanalysisapp/Launcher.java
@@ -1,7 +1,14 @@
+/*Khelifa darine g2
+berrahal maram g2
+oudjeghit safa marwa g2
+ */
+
+
 package com.example.textanalysisapp;
 
 import com.example.textanalysisapp.view.HelloApplication;
 import javafx.application.Application;
+
 
 public class Launcher {
     public static void main(String[] args) {
